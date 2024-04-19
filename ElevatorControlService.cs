9ce36a41834f348fe5eb4ea8779e68b1c91cde23
esbarreiro <erick.barreiro119@gmail.com>
2024-04-19 13:16:01 -0400

    Add project files.

---
+++ ElevatorControlService.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorControlApp
+{
+    public class ElevatorControlService
+    {
+        public int CurrentFloor { get; set; }
+        public Direction ElevatorDirection { get; set; }
+        public bool ElevatorDoorOpen { get; set; }
+
+        private SortedSet<int> ElevatorRequestUp;
+        private SortedSet<int> ElevatorRequestDown;
+
+        public ElevatorControlService()
+        {
+            ElevatorDirection = Direction.NotMoving;
+            CurrentFloor = 1;
+            ElevatorDoorOpen = false;
+            ElevatorRequestDown = new SortedSet<int>();
+            ElevatorRequestUp = new SortedSet<int>();
+        }
+
+        public void CallElevator(int floor)
+        {
+            Direction elevatorDirection = floor > CurrentFloor ? Direction.Up : Direction.Down;
+
+            if (elevatorDirection == Direction.Up)
+                ElevatorRequestUp.Add(floor);
+            else
+                ElevatorRequestDown.Add(floor);
+
+            MoveElevator();
+        }
+
+        private void MoveElevator()
+        {
+            while (ElevatorRequestUp.Count > 0 || ElevatorRequestDown.Count > 0)
+            {
+                if (ElevatorDirection == Direction.Up || ElevatorDirection == Direction.NotMoving)
+                {
+                    if (ElevatorRequestUp.Count > 0)
+                    {
+                        int nextFloor = ElevatorRequestUp.Min;
+                        ElevatorRequestUp.Remove(nextFloor);
+
+                        // this basically simulates that the elevator is going to the next floor
+                        CurrentFloor = nextFloor;
+                    }
+                    else if (ElevatorRequestDown.Count > 0)
+                    {
+                        ElevatorDirection = Direction.Down;
+                    }
+                }
+                else if (ElevatorDirection == Direction.Down)
+                {
+                    if (ElevatorRequestDown.Count > 0)
+                    {
+                        int nextFloor = ElevatorRequestDown.Max;
+                        ElevatorRequestDown.Remove(nextFloor);
+                        // Simulate moving to the next floor
+                        CurrentFloor = nextFloor;
+                    }
+                    else if (ElevatorRequestUp.Count > 0)
+                    {
+                        ElevatorDirection = Direction.Up;
+                    }
+                }
+
+                // Simulate opening doors
+                ElevatorDoorOpen = true;
+                // Simulate waiting for passengers to enter/exit
+                ElevatorDoorOpen = false;
+
+                // If no more requests, hold elevator
+                if (ElevatorRequestUp.Count == 0 && ElevatorRequestDown.Count == 0)
+                    ElevatorDirection = Direction.NotMoving;
+            }
+        }
+        
+
+        public void SelectFloor(int floor)
+        {
+            if (floor > CurrentFloor)
+                ElevatorRequestUp.Add(floor);
+            else if (floor < CurrentFloor)
+                ElevatorRequestDown.Add(floor);
+
+            MoveElevator();
+        }
+
+        public int GetCurrentFloor()
+        {
+            return CurrentFloor;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿namespace ElevatorControlApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to this elevator control! Where would you like to go?");
+            Console.WriteLine("");
+            Console.WriteLine("...................................");
+            ElevatorControlService elevatorControl = new();
+
+            while (true)
+            {
+                Console.WriteLine($"Elevator is currently at floor {elevatorControl.GetCurrentFloor()}");
+                Console.WriteLine("");
+                Console.WriteLine("Please select an option:");
+                Console.WriteLine("1. Call elevator to a floor");
+                Console.WriteLine("2. Inside elevator - Select floor");
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            CallElevator(elevatorControl);
+                            break;
+                        case 2:
+                            InsideElevator(elevatorControl);
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice");
+                            break;
+                    }
+                }
+                else
+                {
+                    InvalidInputResponse();
+                }
+            }
+        }
+
+        static void CallElevator(ElevatorControlService elevatorControl)
+        {
+            Console.WriteLine("Enter the floor to call the elevator:");
+            if (int.TryParse(Console.ReadLine(), out int floor))
+            {
+                try
+                {
+                    ValidateFloor(floor);
+
+                    elevatorControl.SelectFloor(floor);
+                    ElevatorResponse(elevatorControl);
+                }
+                catch (Exception ex)
+                {
+                    ElevatorErrorResponse(ex);
+                }
+            }
+            else
+            {
+                InvalidInputResponse();
+            }
+        }
+
+        static void InsideElevator(ElevatorControlService elevatorControl)
+        {
+            Console.WriteLine("Enter the floor to move to:");
+            if (int.TryParse(Console.ReadLine(), out int floor))
+            {
+                try
+                {
+                    ValidateFloor(floor);
+
+                    elevatorControl.SelectFloor(floor);
+                    ElevatorResponse(elevatorControl);
+
+
+                }
+                catch (Exception ex)
+                {
+                    ElevatorErrorResponse(ex);
+                }
+            }
+            else
+            {
+                InvalidInputResponse();
+            }
+        }
+
+        static void ValidateFloor(int floor)
+        {
+            if (floor < 1 || floor > 5)
+            {
+                throw new Exception("invalid floor selection");
+            }
+        }
+
+        static void ElevatorErrorResponse(Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            Console.WriteLine("");
+        }
+
+        static void ElevatorResponse(ElevatorControlService elevatorControl)
+        {
+            Console.WriteLine($"Ding Dong! Elevator has arrived at floor {elevatorControl.GetCurrentFloor()}");
+            Console.WriteLine("");
+        }
+
+        static void InvalidInputResponse()
+        {
+            Console.WriteLine("Invalid input");
+            Console.WriteLine("");
+        }
+    }
+}
